590f2ba737cdad882e8b4d6324fdb0dc2f3cfebf
Shinji <sjh1803@naver.com>
2024-01-10 17:20:51 +0900

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todolist/screen/main_screen.dart';
+import 'package:todolist/screen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,35 +14,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MainScreen(),
+      initialRoute: '/',
+      routes: {
+        '/':(context) => SplashScreen(),
+        '/main' : (context) => MainScreen(),
+      },
     );
-  }
-}
-
-class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('나의 첫 앱'),),body: Text('안녕하세요'),);
   }
 }
---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+/// 시작 화면(Splash Screen)
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    /// 자동 정렬 -> ctrl + alt + l
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushNamed(context, '/main');
+    },);
+
+    return Scaffold(
+      body: Center(
+        child: Text('시작 화면 입니다.'),
+      ),
+    );
+  }
+}
